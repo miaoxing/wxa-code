a931f9612bce653314c384cff3a1fbec6a1bb80f
twinh <twinhuang@qq.com>
2018-04-25 00:48:48 +0800

    更新场景值

--- src/Controller/WxaCodeLogs.php
+++ src/Controller/WxaCodeLogs.php
@@ -11,6 +11,15 @@ class WxaCodeLogs extends BaseController
         'wxaCodeLogs',
     ];
 
+    protected $validScenes = [
+        1011, // 扫描二维码
+        1012, // 长按图片识别二维码
+        1013, // 手机相册选取二维码
+        1047, // 扫描小程序码
+        1048, // 长按图片识别小程序码
+        1049, // 手机相册选取小程序码
+    ];
+
     public function createAction($req)
     {
         $data = $this->request->getData();
@@ -19,11 +28,19 @@ class WxaCodeLogs extends BaseController
 
         $this->logger->info('上报小程序onShow', $this->request->getContent());
 
+        if (!in_array($req['scene'], $this->validScenes)) {
+            return $this->err('场景不符合');
+        }
+
         // 统一使用绝对的路径
-        $path = '/' . $req['path'];
+        $data = [];
+        foreach ($req['query'] as $key => $value) {
+            $data[$key] = urldecode($value);
+        }
+        $path = $this->url->append('/' . $req['path'], $data);
         $wxaCode = wei()->wxaCodeModel()->find(['path' => $path]);
         if (!$wxaCode) {
-            return $this->err('小程序码不存在');
+            return $this->err('小程序码不存在' . $path);
         }
 
         $user = wei()->user()->find(['wechatOpenId' => $req['wechatOpenId']]);
