0938f398c291dc333420306b2843b15c5eee67f6
twinh <twinhuang@qq.com>
2019-02-12 14:06:17 +0800

    refactor: modal更新

--- resources/views/admin/_codeModal.php
+++ resources/views/admin/_codeModal.php
@@ -4,10 +4,10 @@
   <div class="modal-dialog">
     <div class="modal-content">
       <div class="modal-header">
-        <button type="button" class="close" data-dismiss="modal">
-          <span aria-hidden="true">&times;</span><span class="sr-only">Close</span>
+        <h5 class="modal-title" id="wxa-code-modal-label">查看小程序码</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
         </button>
-        <h4 class="modal-title" id="wxa-code-modal-label">查看小程序码</h4>
       </div>
       <div class="modal-body text-center">
         <img class="js-wxa-code-image img-max" src="">
